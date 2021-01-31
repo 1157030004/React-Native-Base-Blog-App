1961450b70e299a20685b4ba087befee28253b21
Ariq Dhia Irfanudin <invanteris@gmail.com>
2021-01-31 16:49:43 +0700

    Displaying a form

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,14 +1,42 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 const CreateScreen = () => {
-    return (
-        <View>
-            <Text>Create Screen</Text>
-        </View>
-    )
-}
+	const [title, setTitle] = useState("");
+	const [content, setContent] = useState("");
+	return (
+		<View>
+			<Text style={styles.label}>Your Title</Text>
+			<TextInput
+				style={styles.input}
+				value={title}
+				onChangeText={(text) => setTitle(text)}
+			/>
+			<Text style={styles.label}>Your Content</Text>
+			<TextInput
+				style={styles.input}
+				value={content}
+				onChangeText={(text) => setContent(text)}
+			/>
+			<Button title="Publish" />
+		</View>
+	);
+};
 
-export default CreateScreen
+export default CreateScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+	input: {
+		fontSize: 18,
+		borderWidth: 1,
+		borderColor: "gray",
+		marginBottom: 15,
+		padding: 5,
+		margin: 5,
+	},
+	label: {
+		fontSize: 20,
+		marginBottom: 5,
+		marginLeft: 5,
+	},
+});
