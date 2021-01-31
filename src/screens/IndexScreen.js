a3d9b65e69884bdf3bfb3d6e92a1e188653e336e
Ariq Dhia Irfanudin <invanteris@gmail.com>
2021-01-31 16:10:32 +0700

    A bit styling

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,18 +1,23 @@
 import React, { useContext } from "react";
 import { StyleSheet, Text, View, FlatList, Button } from "react-native";
-import {Context} from "../context/BlogContext";
+import { Context } from "../context/BlogContext";
+import { AntDesign } from "@expo/vector-icons";
 
 const IndexScreen = () => {
 	const { state, addBlogPost } = useContext(Context);
 	return (
 		<View>
-			<Text>Index Screen</Text>
 			<Button title="Add Blog Post" onPress={addBlogPost} />
 			<FlatList
 				data={state}
 				keyExtractor={(blogPosts) => blogPosts.title}
 				renderItem={({ item }) => {
-					return <Text>{item.title}</Text>;
+					return (
+						<View style={styles.row}>
+							<Text style={styles.title}>{item.title}</Text>
+							<AntDesign style={styles.icon} name="delete" />
+						</View>
+					);
 				}}
 			/>
 		</View>
@@ -21,4 +26,19 @@ const IndexScreen = () => {
 
 export default IndexScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	row: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+		borderTopWidth: 1,
+		borderColor: "gray",
+	},
+	title: {
+		fontSize: 18,
+	},
+	icon: {
+		fontSize: 24,
+	},
+});
